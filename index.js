04e030cd9c1c61bfcaebe26dbff0d96c67835d80
nellanjimms <wambui.ndungu2019@students.jkuat.ac.ke>
2023-09-14 11:18:43 +0300

    Added .js file

---
+++ index.js
@@ -0,0 +1,16 @@
+//Get access to elements
+const userForm = document.querySelector('form');
+const firstNameInput = document.getElementById('firstName');
+const lastNameInput = document.getElementById('lastName');
+const submitButton = document.querySelector('button');
+const sideBar = document.querySelector('aside');
+
+
+//Create event listener
+submitButton.addEventListener ('click', ($event) => {
+    $event.preventDefault();
+    sideBar.textContent = 'Welcome ' + firstNameInput.value + ' ' +
+lastNameInput.value + '. It is good to see you.';
+userForm.reset();
+
+})
