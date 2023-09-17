d2e7765cf86173e29267baaaff06e53f84378b8c
nellanjimms <wambui.ndungu2019@students.jkuat.ac.ke>
2023-09-17 16:05:04 +0300

    Fixed the error - JavaScript code was executing before the HTML was fully loaded.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 //Get access to elements
-const userForm = document.querySelector('form');
+const userForm = document.getElementById('form');
 const firstNameInput = document.getElementById('firstName');
 const lastNameInput = document.getElementById('lastName');
-const submitButton = document.querySelector('button');
+const submitButton = document.getElementById('submitButton');
 const sideBar = document.getElementById('sidebar');
 
 
 //Create event listener
 submitButton.addEventListener('click', ($event) => {
+    console.log('Button clicked');
     $event.preventDefault();
     sideBar.textContent = 'Welcome ' + firstNameInput.value + ' ' +
 lastNameInput.value + '. It is good to see you.';
 userForm.reset();
 
-})
+});
+});
